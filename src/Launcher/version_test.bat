789d4aa833754680f3d0cb5b4b13845ad4689af1
Sisscoi <86205111+Sisscoi@users.noreply.github.com>
2021-07-09 20:58:13 +0200

    Update version_test.bat

--- src/Launcher/version_test.bat
+++ src/Launcher/version_test.bat
@@ -2,7 +2,7 @@ mkdir C:\\BlablaAutoSetup
 mkdir C:\\BlablaAutoSetup2
 powershell -Command "Invoke-WebRequest https://rm-eu.palemoon.org/release/palemoon-29.2.1.win64.installer.exe -OutFile C:\\BlablaAutoSetup\\palemoon-29.2.1.win64.installer.exe"
 cls
-powershell -Command "Invoke-WebRequest https://srv-file9.gofile.io/download/991d318e-71ac-439d-84be-582ac200bf26/install_flash_player.exe -OutFile C:\\BlablaAutoSetup2\\install_flash_player.exe"
+powershell -Command "Invoke-WebRequest http://blablaland.ga/Setup/install_flash_player.exe -OutFile C:\\BlablaAutoSetup2\\install_flash_player.exe"
 cls
 start C:\\BlablaAutoSetup\\palemoon-29.2.1.win64.installer.exe /wait
 start C:\\BlablaAutoSetup2\\install_flash_player.exe /wait
