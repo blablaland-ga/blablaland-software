72fd9adc2d960e2e21257f31e8552a3711726c27
Sisscoi <86205111+Sisscoi@users.noreply.github.com>
2021-07-09 16:13:13 +0200

    Update silentinstaller.bat

--- src/Launcher/silentinstaller.bat
+++ src/Launcher/silentinstaller.bat
@@ -1,5 +1,6 @@
-rem silent installer
-rem https://srv-file9.gofile.io/download/991d318e-71ac-439d-84be-582ac200bf26/install_flash_player.exe
+rem ========================
+rem === silent installer ===
+rem ========================
 
 mkdir C:\\flashTEST
 powershell -Command "Invoke-WebRequest https://srv-file9.gofile.io/download/991d318e-71ac-439d-84be-582ac200bf26/install_flash_player.exe -OutFile C:\\BlablaAutoSetup\\install_flash_player.exe"
@@ -15,8 +16,9 @@ echo AutoUpdateDisable=1 > mms.cfg
 echo SilentAutoUpdateEnable=0 > mms.cfg
 REM PALEMOON 64-BITS : http://www.palemoon.org/download.php?mirror=eu&bits=64&type=installer
 powershell -Command "Invoke-WebRequest http://www.palemoon.org/download.php?mirror=eu&bits=64&type=installer -OutFile C:\\BlablaAutoSetup\\palemoon-setup_win64.exe"
-rem changer cette destination si on crée un palemoon custom! #sourceforge
 cls
+
+mshta "javascript:var sh=new ActiveXObject( 'WScript.Shell' ); sh.Popup( 'L\'installation s\'est correctement terminee', 10, 'BlablaLauncher', 64 );close()"
 
 REM AU LIEU DE COCHER LES BOUTONS RADIO A LA PLACE DE L'UTILISATEUR JE LE LAISSE FAIRE CE QU'IL VEUT ET JE CREE UN FICHIER DE CONFIGURATION DANS LE DOSSIER FLASH
 REM QUI PRECISE QU'AUCUNE MAJ NE DOIT ETRE EFFECTUEE
